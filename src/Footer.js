ee47e456c231b6228ac33b21dbea8e1632b0646c
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2022-12-25 16:27:10 -0800

    Added Header, Nav, Main, and Footer components and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,56 @@
+function Footer() {
+    return (
+        <footer>
+            <img src="https://www.48hourslogo.com/oss/works/2022/01/12/135523929088/115168_45900_d4d2e8ab-0a96-4bc4-ae9d-f9898dc70956.jpg"></img>
+            <ul>
+                <p>Doormat Navigation</p>
+                <li>
+                    <a href="home.html">Home</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="menu.html">Menu</a>
+                </li>
+                <li>
+                    <a href="reservations.html">Reservations</a>
+                </li>
+                <li>
+                    <a href="orderonline.html">Order Online</a>
+                </li>
+                <li>
+                    <a href="login.html">Login</a>
+                </li>
+            </ul>
+
+            <ul>
+                <p>Contact</p>
+                <li>
+                    <a href="address.html">Address</a>
+                </li>
+                <li>
+                    <a href="phonenumber.html">Phone Number</a>
+                </li>
+                <li>
+                    <a href="email.html">Email</a>
+                </li>
+            </ul>
+
+            <ul>
+                <p>Social media Links</p>
+                <li>
+                    <a href="address.html">Address</a>
+                </li>
+                <li>
+                    <a href="phonenumber.html">Phone Number</a>
+                </li>
+                <li>
+                    <a href="email.html">Email</a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+function Nav() {
+    return (
+        <nav>
+            <img src="https://www.48hourslogo.com/oss/works/2022/01/12/135523929088/115168_45900_d4d2e8ab-0a96-4bc4-ae9d-f9898dc70956.jpg"></img>
+            <ul>
+                <li>
+                    <a href="home.html">Home</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="menu.html">Menu</a>
+                </li>
+                <li>
+                    <a href="reservations.html">Reservations</a>
+                </li>
+                <li>
+                    <a href="orderonline.html">Order Online</a>
+                </li>
+                <li>
+                    <a href="login.html">Login</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
